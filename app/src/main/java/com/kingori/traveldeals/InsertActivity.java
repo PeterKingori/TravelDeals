beae80ed423028d6494c5d0f353ed5c062a74bee
Peter Kingori <kpndegwa@gmail.com>
2020-07-15 01:38:20 +0300

    Add the TravelDeal.java class that will be used to create deal objects. Also add code to access the EditText views in the insert_activity.xml layout file, get the user input and save it as a deal object in Firebase realtime database using the save option in the menu.

--- app/src/main/java/com/kingori/traveldeals/InsertActivity.java
+++ app/src/main/java/com/kingori/traveldeals/InsertActivity.java
@@ -1,10 +1,14 @@
 package com.kingori.traveldeals;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -12,6 +16,9 @@ import com.google.firebase.database.FirebaseDatabase;
 public class InsertActivity extends AppCompatActivity {
     private FirebaseDatabase mFirebaseDatabase;  // This is the entry point for accessing a Firebase Realtime Database
     private DatabaseReference mDatabaseReference;  // This gets a reference to the location in the database where you will read/write data
+    EditText txtTitle;
+    EditText txtDescription;
+    EditText txtPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,10 @@ public class InsertActivity extends AppCompatActivity {
         setContentView(R.layout.activity_insert);
         mFirebaseDatabase = FirebaseDatabase.getInstance();
         mDatabaseReference = mFirebaseDatabase.getReference().child("traveldeals");
+        txtTitle = findViewById(R.id.txtTitle);
+        txtDescription = findViewById(R.id.txtDescription);
+        txtPrice = findViewById(R.id.txtPrice);
+
     }
 
     @Override
@@ -26,6 +37,35 @@ public class InsertActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.save_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_menu:
+                saveDeal();
+                Toast.makeText(this, "Deal saved", Toast.LENGTH_SHORT).show();
+                clean();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void saveDeal() {
+        String title = txtTitle.getText().toString();
+        String description = txtDescription.getText().toString();
+        String price = txtPrice.getText().toString();
+
+        TravelDeal deal = new TravelDeal(title, description, price, "");
+        mDatabaseReference.push().setValue(deal);
+    }
+
+    private void clean() {
+        txtTitle.setText("");
+        txtDescription.setText("");
+        txtPrice.setText("");
+        txtTitle.requestFocus();
     }
 
 }
